245b3f4d0da1aec0a01079c630e8800986f34bec
Leonid Zakharov <lez@kodix.ru>
2021-02-02 11:42:41 +0300

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "@/assets/css/styles.css";
 
 const App: React.VFC = (): JSX.Element => {
   return <div>React Admin Panel is here</div>;
--- test/npm-npx/README.md
+++
@@ -1,4 +0,0 @@
-# OD React SB
-
-### SVG sprite generation via `svg-sprite-generator`
-`svg-sprite-generate -d assets/icons -o assets/images/sprite.svg ` 
